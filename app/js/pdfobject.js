aea966a134beff0e3cda70f2ac2bee1761772ace
Stephen Young <StephenJayYoung@gmail.com>
2014-12-26 18:03:12 -0500

    started messing with social buttons on contact page

--- app/js/pdfobject.js
+++
@@ -1,7 +0,0 @@
-var myPDF = new PDFObject({ url: "sample.pdf" }).embed();
-
-var variablename = new PDFObject({ url: "Stephen_Young_Resume.pdf" }).embed("targetElementID");
-
-if(myPDF){
-  //do something
-}
